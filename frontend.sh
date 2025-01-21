0b21135c3636059af05a5290376ff4a5f48efe98
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-01-22 00:07:50 +0530

    shell script

--- frontend.sh
+++ frontend.sh
@@ -33,24 +33,24 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 CHECK_ROOT
 
 dnf install nginx -y  &>>$LOG_FILE_NAME
-    validate $? " installing nginx "
+    VALIDATE $? " installing nginx "
 
 systemctl enable nginx &>>$LOG_FILE_NAME
-    validate $? " enabling nginx "
+    VALIDATE $? " enabling nginx "
 
 systemctl start nginx &>>$LOG_FILE_NAME
-    validate $? " starting nginx "
+    VALIDATE $? " starting nginx "
 
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE_NAME
-    validate $? " downloading the code "
+    VALIDATE $? " downloading the code "
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
-    validate $? " unzip frontend "
+    VALIDATE $? " unzip frontend "
 
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf 
 
 systemctl restart nginx &>>$LOG_FILE_NAME
-    validate $? " restart the nginx server "
+    VALIDATE $? " restart the nginx server "
