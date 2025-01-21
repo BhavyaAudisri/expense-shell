7bccb6b7aa9657ee6c136a447d5cb7d966c39a7d
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-01-21 23:18:05 +0530

    shell script

--- mysql.sh
+++ mysql.sh
@@ -41,7 +41,7 @@ systemctl enable mysqld &>>$LOG_FILE_NAME
 systemctl start mysqld &>>$LOG_FILE_NAME
     VALIDATE $? "Starting MySQL Server"
 
-mysql -h mysql.somisettibhavya.life -u root -pExpenseApp@1 -e 'show databases'; &>>$LOG_FILE_NAME
+mysql -h mysql.somisettibhavya.life -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
