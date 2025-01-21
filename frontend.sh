acd748fcc35ebc527b95a4992a5a894d66836175
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-01-22 00:29:37 +0530

    shell script

--- frontend.sh
+++ frontend.sh
@@ -49,10 +49,13 @@ curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expe
     VALIDATE $? " downloading the code "
 
 cd /usr/share/nginx/html
+    VALIDATE $? "Moving to HTML directory"
+
 unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
     VALIDATE $? " unzip frontend "
 
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf 
-
+    VALIDATE $? "Copied expense config"
+    
 systemctl restart nginx &>>$LOG_FILE_NAME
     VALIDATE $? " restart the nginx server "
